1bc7f928afd7d1d63e3d09e47c04f50fb7fc30bd
Chopperla Santosh <chopperla2005@gmail.com>
2026-02-09 22:24:34 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Santosh%402005@localhost/FastApi'
+SQLALCHEMY_DATABASE_URL = '####'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
